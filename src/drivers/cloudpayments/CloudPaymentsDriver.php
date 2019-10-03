836d57ff7515557d6c643ce7f46104475e9fb994
SergioMadness <madness.sergio@gmail.com>
2019-10-03 17:02:11 +0300

    needForm as useWidget

--- src/drivers/cloudpayments/CloudPaymentsDriver.php
+++ src/drivers/cloudpayments/CloudPaymentsDriver.php
@@ -129,7 +129,7 @@ class CloudPaymentsDriver implements PayService, CloudPaymentsService, Recurring
      */
     public function needForm(): bool
     {
-        return false;
+        return $this->getUseWidget();
     }
 
     /**
